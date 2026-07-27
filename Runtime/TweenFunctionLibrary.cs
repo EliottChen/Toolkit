4c58a79cc199e4109358f8f2c7b803c5849796d4
Eliott <tan.eliott04@gmail.com>
2026-07-28 00:32:37 +0200

    feat(tween): add tween function library

---
+++ Runtime/TweenFunctionLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EliottChen.Toolkit
+{
+    public static class TweenFunctionLibrary
+    {
+        /// <summary>
+        /// Transform a ratio in a ease out cubic function, where x is the ratio
+        /// </summary>
+        /// <param name="x"> should be normalized between 0 and 1</param>
+        /// <returns></returns>
+        static public float EaseOutCubic(float x)
+        {
+            return 1f - (float)Math.Pow(1f - x, 3f);
+        }
+
+        static public float EaseOutQuint(float x)
+        {
+            return 1 - (float)MathF.Pow(1f - x, 5);
+        }
+    }
+}
